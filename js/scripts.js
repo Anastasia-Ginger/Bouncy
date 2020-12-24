4f42dda124de42a69a7b648aa8e092d161dbed82
Anastasia-Ginger <kurenkova0301@gmail.com>
2020-12-24 20:22:17 +0300

    add section team

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
+$(document).ready(function () {
+    $('.nav-toggle').click(function () {
+        var collapse_content_selector = $(this).attr('href');
+        var toggle_switch = $(this);
+        $(collapse_content_selector).toggle(function () {
+            if ($(this).css('display') == 'none') {
+                toggle_switch.html('+Read More');
+            } else {
+                toggle_switch.html('Hide text');
+            }
+        });
+    });
+});
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         loop:true,
